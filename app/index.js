5e77a82c7d5855ca4cd9b0a2fc26191976edf4bb
Kochev7 <akimkochev7gmail@.com>
2022-04-27 23:03:42 +0200

    some updates

--- app/index.js
+++ app/index.js
@@ -1,7 +1,17 @@
 import gsap from 'gsap';
 
+import Home from './pages/Home/index.js';
+
 class App {
-	constructor() {}
+	constructor() {
+		this.createPages();
+	}
+
+	createPages() {
+		this.pages = {
+			home: new Home(),
+		};
+	}
 }
 
 new App();
--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,5 +1,16 @@
 import gsap from 'gsap';
 
 export default class Home {
-	constructor() {}
+	constructor() {
+		this.clipTitle();
+	}
+	clipTitle() {
+		// const titleWrapper = document.querySelector('.home__article__title__wrapper');
+		// console.log(titleWrapper);
+		// const titles = [...titleWrapper.querySelectorAll('h2')];
+		// const height = titles.map((t) => t.getBoundingClientRect().height);
+		// console.log(height);
+		// titleWrapper.style.height = height[0] * 2 - height[1];
+		// titleWrapper.style.overflow = 'hidden';
+	}
 }
