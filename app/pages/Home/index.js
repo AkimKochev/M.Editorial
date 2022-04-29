efa0f74e826486dea18e3134f85f888efe9ef4cf
Kochev7 <akimkochev7gmail@.com>
2022-04-30 00:24:32 +0200

    Updates

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -2,15 +2,50 @@ import gsap from 'gsap';
 
 export default class Home {
 	constructor() {
-		this.clipTitle();
+		const ids = [...document.querySelectorAll('[data-article-id="2"]')];
+
+		ids.forEach((id) => {
+			const child = id.children[0] ? id.children[0] : id;
+			child.style.transform = 'translateY(100%)';
+		});
+
+		this.cursorInner = document.querySelector('.cursor__inner');
+		this.cursorOuter = document.querySelector('.cursor__outer');
+		window.addEventListener('mousemove', this.mouseMove.bind(this));
+
+		this.mousePos = {
+			currentX: 0,
+			targetX: 0,
+			currentY: 0,
+			targetY: 0,
+			ease: 0.04,
+		};
 	}
-	clipTitle() {
-		// const titleWrapper = document.querySelector('.home__article__title__wrapper');
-		// console.log(titleWrapper);
-		// const titles = [...titleWrapper.querySelectorAll('h2')];
-		// const height = titles.map((t) => t.getBoundingClientRect().height);
-		// console.log(height);
-		// titleWrapper.style.height = height[0] * 2 - height[1];
-		// titleWrapper.style.overflow = 'hidden';
+
+	interpolate(current, target, ease) {
+		return current * (1 - ease) + ease * target;
+	}
+
+	mouseMove(event) {
+		this.mousePos.targetX = event.clientX;
+		this.mousePos.targetY = event.clientY;
+		this.animate();
+	}
+
+	animate() {
+		this.mousePos.currentX = this.interpolate(
+			this.mousePos.currentX,
+			this.mousePos.targetX,
+			this.mousePos.ease
+		).toFixed(2);
+		this.mousePos.currentY = this.interpolate(
+			this.mousePos.currentY,
+			this.mousePos.targetY,
+			this.mousePos.ease
+		).toFixed(2);
+		this.cursorInner.style.transform = `translate3d(${this.mousePos.currentX}px, ${this.mousePos.currentY}px, 0)`;
+
+		this.cursorOuter.style.transform = `translate3d(${this.mousePos.currentX}px, ${this.mousePos.currentY}px, 0)`;
+		requestAnimationFrame(this.animate.bind(this));
 	}
 }
