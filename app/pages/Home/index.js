53d2c9a37cf75993fab4c1847ec8e9a5d789f57b
Kochev7 <akimkochev7gmail@.com>
2022-05-05 22:49:50 +0200

    Raw animations

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -10,7 +10,7 @@ export default class Home {
 		};
 
 		this.splitText();
-		this.changeArticle({scrollTo: 100, startFrom: 2});
+		this.changeArticle({scrollTo: -100, startFrom: 2});
 
 		window.addEventListener('wheel', this.scrollWheelHandler.bind(this));
 	}
@@ -47,26 +47,79 @@ export default class Home {
 		}
 	}
 
+	resetTransform(element) {
+		return new Promise((resolve) => {
+			element.style.visibility = 'hidden';
+			element.style.opacity = '0';
+			element.style.transition = `transform .1s`;
+			element.style.transform = `translate3d(0%, 100%, 0)`;
+			element.addEventListener('transitionend', (_) => {
+				element.style.visibility = 'visible';
+				element.style.opacity = '1';
+				resolve();
+			});
+		});
+	}
+
 	changeArticle({scrollTo, startFrom = 1}) {
+		const previusArticle = document.querySelector(
+			`[data-article-id="${[
+				this.data.currentIdArticle - 1 < 1 ? 1 : this.data.currentIdArticle - 1,
+			]}"]`
+		);
+		const nextArticle = document.querySelector(
+			`[data-article-id="${[
+				this.data.currentIdArticle + 1 > this.data.articles.length
+					? this.data.articles.length
+					: this.data.currentIdArticle + 1,
+			]}"]`
+		);
+		const currentArticle = document.querySelector(
+			`[data-article-id="${[this.data.currentIdArticle]}"]`
+		);
+		previusArticle.style.zIndex = 1;
+		nextArticle.style.zIndex = 1;
+		currentArticle.style.zIndex = 2;
+
 		for (let i = startFrom; i <= this.data.articles.length; i++) {
 			const currentIdArticle = `[data-article-id="${[i]}"]`;
-
 			const articleElements = {
 				p: [...document.querySelectorAll(`${currentIdArticle} p`)],
 				h2: [...document.querySelectorAll(`${currentIdArticle} h2`)],
 				img: [...document.querySelectorAll(`${currentIdArticle} img`)],
 			};
+			console.log(i);
 
 			for (const elements in articleElements) {
-				articleElements[elements].forEach((el) => {
-					if (el.tagName === 'IMG')
+				articleElements[elements].forEach(async (el) => {
+					if (el.tagName === 'IMG') {
+						if (
+							currentIdArticle === `[data-article-id="${this.data.currentIdArticle}"]`
+						)
+							await this.resetTransform(el);
+
+						el.style.transition = `transform 1.5s ${
+							this.data.currentIdArticle === i ? 0 : 0.6
+						}s cubic-bezier(0.55, 0, 0.175, 1)`;
 						el.style.transform = `translate3d(0, ${
 							scrollTo * (this.data.currentIdArticle - i)
 						}%, 0)`;
-
+					}
 					[...el.children].forEach((span) => {
 						if (span.tagName === 'BR') return;
-						[...span.children].forEach((spanElement) => {
+
+						[...span.children].forEach(async (spanElement) => {
+							if (
+								currentIdArticle === `[data-article-id="${this.data.currentIdArticle}"]`
+							)
+								await this.resetTransform(spanElement);
+
+							spanElement.style.transition = `transform 1.5s ${
+								this.data.currentIdArticle - 1 === i ||
+								this.data.currentIdArticle + 1 === i
+									? 0
+									: 0.6
+							}s cubic-bezier(0.55, 0, 0.175, 1)`;
 							spanElement.style.transform = `translate3d(0, ${
 								scrollTo * (this.data.currentIdArticle - i)
 							}%, 0)`;
